023f0f4180f593f80bdcc9f83fa694b71f0da2c7
Yuriy Yangel <arlosaneq@gmail.com>
2024-04-13 18:58:34 +0700

    Added config

--- AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788ec184ee9c0fd1d188aa4a2fc873226627bd2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+626cc7b05e798ad5f4572ba405af8e0d05a19973")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
