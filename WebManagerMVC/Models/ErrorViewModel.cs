700fb0b6a96f3f1ff883fe84a037ceca1525aa4c
Yuriy Yangel <arlosaneq@gmail.com>
2024-04-16 19:49:24 +0700

    Added MVC

---
+++ WebManagerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebManagerMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
