5bd2563d547cee0fe8da1da66f5574ddc2817bd6
Yuriy Yangel <arlosaneq@gmail.com>
2024-04-17 19:12:20 +0700

    Fixed connection string

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -2,7 +2,7 @@ using EasyNetQ;
 using NotificationService.Application;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IBus>(RabbitHutch.CreateBus("host=localhost;username=rmuser;password=rmpassword"));
+builder.Services.AddSingleton<IBus>(RabbitHutch.CreateBus(builder.Configuration.GetConnectionString("EasyNetQ")));
 builder.Services.AddHostedService<NotificationBackgroundService>();
 var app = builder.Build();
 app.UseHttpsRedirection();
