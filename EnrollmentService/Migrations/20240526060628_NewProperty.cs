2186b95afe7fa28d06cb159cccd3eb2497cc3437
Yuriy Yangel <arlosaneq@gmail.com>
2024-05-26 14:10:32 +0700

    Migrations

---
+++ EnrollmentService/Migrations/20240526060628_NewProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnrollmentService.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Document",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Document");
+        }
+    }
+}
